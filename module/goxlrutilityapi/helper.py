6e0dd113d54244f1e793944b3a615780c451a1da
Aidan Timson <contact@timmo.xyz>
2023-04-29 00:19:06 +0100

    Optional

--- module/goxlrutilityapi/helper.py
+++ module/goxlrutilityapi/helper.py
@@ -1,9 +1,11 @@
 """GoXLR Utility API: Helper"""
 from __future__ import annotations
 
+from typing import Optional
+
 from .models.status import Mixer, Status
 
 
-async def get_mixer_from_status(status: Status) -> Mixer:
+async def get_mixer_from_status(status: Status) -> Optional[Mixer]:
     """Get first mixer from status"""
-    return next(iter(status.mixers.values()))
+    return next(iter(status.mixers.values()), None)
