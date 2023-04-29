38814c6b325bd27a4348b0b5aab165cec7b77976
Aidan Timson <contact@timmo.xyz>
2023-04-29 02:55:48 +0100

    Add get volume percentage helper

--- module/goxlrutilityapi/const.py
+++ module/goxlrutilityapi/const.py
@@ -8,14 +8,19 @@ from .models.request import Request
 from .models.response import Response
 from .models.status import Status
 
-# General
+# Connection
 DEFAULT_HOST: Final[str] = "localhost"
 DEFAULT_PORT: Final[int] = 14564
 
-# API
+# Mixer
+VOLUME_MAX: Final[int] = 254
+
+# Request/Response Keys
 KEY_DATA: Final[str] = "data"
 KEY_ID: Final[str] = "id"
 KEY_TYPE: Final[str] = "type"
+
+# Request/Response Types
 REQUEST_TYPE_GET_STATUS: Final[str] = "GetStatus"
 RESPONSE_TYPE_PATCH: Final[str] = "Patch"
 RESPONSE_TYPE_STATUS: Final[str] = "Status"
--- module/goxlrutilityapi/helper.py
+++ module/goxlrutilityapi/helper.py
@@ -3,9 +3,20 @@ from __future__ import annotations
 
 from typing import Optional
 
+from .const import VOLUME_MAX
 from .models.status import Mixer, Status
 
 
 async def get_mixer_from_status(status: Status) -> Optional[Mixer]:
     """Get first mixer from status"""
     return next(iter(status.mixers.values()), None)
+
+
+def get_volume_percentage(
+    mixer: Mixer,
+    key: str,
+) -> int | None:
+    """Return the volume in percent."""
+    if (value := getattr(mixer.levels.volumes, key, None)) is not None:
+        return round(value / VOLUME_MAX * 100)
+    return None
