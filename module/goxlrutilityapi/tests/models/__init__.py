9385ae4a40a4d585718f1f6d690dabeb32d25c2c
Aidan Timson <contact@timmo.xyz>
2023-05-04 21:29:43 +0100

    Create model tests

---
+++ module/goxlrutilityapi/tests/models/__init__.py
@@ -0,0 +1,19 @@
+"""GoXLR Utility API: Tests - Models"""
+from polyfactory.factories.pydantic_factory import ModelFactory
+from polyfactory.pytest_plugin import register_fixture
+
+from ...models import DefaultBaseModel
+
+
+@register_fixture
+class DefaultBaseModelFactory(ModelFactory[DefaultBaseModel]):
+    """DefaultBaseModel Factory"""
+
+    __model__ = DefaultBaseModel
+
+
+def test_default_base_model() -> None:
+    """Test DefaultBaseModel"""
+    model = DefaultBaseModelFactory.build()
+
+    assert isinstance(model, DefaultBaseModel)
---
+++ module/goxlrutilityapi/tests/models/map_item.py
@@ -0,0 +1,23 @@
+"""GoXLR Utility API: Tests - MapItem Models"""
+from polyfactory.factories.pydantic_factory import ModelFactory
+from polyfactory.pytest_plugin import register_fixture
+
+from ...models.map_item import MapItem
+
+
+@register_fixture
+class MapItemFactory(ModelFactory[MapItem]):
+    """MapItem Factory"""
+
+    __model__ = MapItem
+
+    name = "Name"
+    icon = "mdi:icon"
+
+def test_map_item() -> None:
+    """Test MapItem"""
+    model = MapItemFactory.build()
+
+    assert isinstance(model, MapItem)
+    assert model.name == "Name"
+    assert model.icon == "mdi:icon"
