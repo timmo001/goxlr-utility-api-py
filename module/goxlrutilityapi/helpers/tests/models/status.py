7eb1a08c80a2591d18b74a90a9caecc97bb469b2
Aidan Timson <contact@timmo.xyz>
2023-05-05 15:32:51 +0100

    Update status test helper (#16)

--- module/goxlrutilityapi/helpers/tests/models/status.py
+++ module/goxlrutilityapi/helpers/tests/models/status.py
@@ -1,19 +1,47 @@
 """GoXLR Utility API: Tests helpers - Status Models"""
+from typing import Final
+
 from polyfactory import Fixture
 from polyfactory.factories.pydantic_factory import ModelFactory
-from polyfactory.pytest_plugin import register_fixture
 
-from ....models.status import Config, Files, Mixer, Paths, Status
+from ....models.status import Config, Files, Hardware, Mixer, Paths, Status, UsbDevice
+
+FIXTURE_SERIAL: Final[str] = "abc123"
+FIXTURE_MANUFACTURER_NAME: Final[str] = "Name of the manufacturer"
+FIXTURE_NAME: Final[str] = "Name of the device"
+
+class UsbDeviceFactory(ModelFactory[UsbDevice]):
+    """USB Device Factory"""
+
+    __model__ = UsbDevice
+
+    manufacturer_name = FIXTURE_MANUFACTURER_NAME
+    product_name = FIXTURE_NAME
 
 
-@register_fixture
+class HardwareFactory(ModelFactory[Hardware]):
+    """Hardware Factory"""
+
+    __model__ = Hardware
+
+    usb = UsbDeviceFactory().build()
+
+
+class MixerFactory(ModelFactory[Mixer]):
+    """Mixer Factory"""
+
+    __model__ = Mixer
+
+    hardware = HardwareFactory().build()
+
+
 class StatusFactory(ModelFactory[Status]):
     """Status Factory"""
 
     __model__ = Status
 
     config = Fixture(Config)
-    mixers = Fixture(dict[str, Mixer])
+    mixers = {FIXTURE_SERIAL: MixerFactory().build()}
     paths = Fixture(Paths)
     files = Fixture(Files)
 
