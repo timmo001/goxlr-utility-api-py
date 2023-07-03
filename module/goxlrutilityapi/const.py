db173bc584be7fd05633454ec96318145ececac1
Aidan Timson <contact@timmo.xyz>
2023-07-03 16:30:51 +0100

    Lint

--- module/goxlrutilityapi/const.py
+++ module/goxlrutilityapi/const.py
@@ -148,64 +148,154 @@ NAME_MAP: Final[dict[str, MapItem]] = {
     "cough": MapItem(key="Cough", name=BUTTON_COUGH, icon="mdi:microphone-off"),
     "d": MapItem(key="D", name="Fader 4", icon="mdi:tune-vertical-variant"),
     "D": MapItem(key="D", name="Fader 4", icon="mdi:tune-vertical-variant"),
-    "effect_fx": MapItem(key="effect_fx", name="Effect FX", icon="mdi:equalizer-outline"),
-    "effect_hard_tune": MapItem(key="effect_hard_tune", name="Effect Hard Tune", icon="mdi:knob"),
-    "effect_megaphone": MapItem(key="effect_megaphone", name="Effect Megaphone", icon="mdi:bullhorn-outline"),
-    "effect_robot": MapItem(key="effect_robot", name="Effect Robot", icon="mdi:robot-outline"),
-    "effect_select_1": MapItem(key="effect_select_1", name="Effect Select 1", icon="mdi:sawtooth-wave"),
-    "effect_select_2": MapItem(key="effect_select_2", name="Effect Select 2", icon="mdi:sawtooth-wave"),
-    "effect_select_3": MapItem(key="effect_select_3", name="Effect Select 3", icon="mdi:sawtooth-wave"),
-    "effect_select_4": MapItem(key="effect_select_4", name="Effect Select 4", icon="mdi:sawtooth-wave"),
-    "effect_select_5": MapItem(key="effect_select_5", name="Effect Select 5", icon="mdi:sawtooth-wave"),
-    "effect_select_6": MapItem(key="effect_select_6", name="Effect Select 6", icon="mdi:sawtooth-wave"),
-    "effect_select1": MapItem(key="effect_select1", name="Effect Select 1", icon="mdi:sawtooth-wave"),
-    "effect_select2": MapItem(key="effect_select2", name="Effect Select 2", icon="mdi:sawtooth-wave"),
-    "effect_select3": MapItem(key="effect_select3", name="Effect Select 3", icon="mdi:sawtooth-wave"),
-    "effect_select4": MapItem(key="effect_select4", name="Effect Select 4", icon="mdi:sawtooth-wave"),
-    "effect_select5": MapItem(key="effect_select5", name="Effect Select 5", icon="mdi:sawtooth-wave"),
-    "effect_select6": MapItem(key="effect_select6", name="Effect Select 6", icon="mdi:sawtooth-wave"),
-    "fader_1_mute": MapItem(key="fader_1_mute", name="Fader 1 Mute", icon="mdi:microphone-off"),
+    "effect_fx": MapItem(
+        key="effect_fx", name="Effect FX", icon="mdi:equalizer-outline"
+    ),
+    "effect_hard_tune": MapItem(
+        key="effect_hard_tune", name="Effect Hard Tune", icon="mdi:knob"
+    ),
+    "effect_megaphone": MapItem(
+        key="effect_megaphone", name="Effect Megaphone", icon="mdi:bullhorn-outline"
+    ),
+    "effect_robot": MapItem(
+        key="effect_robot", name="Effect Robot", icon="mdi:robot-outline"
+    ),
+    "effect_select_1": MapItem(
+        key="effect_select_1", name="Effect Select 1", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select_2": MapItem(
+        key="effect_select_2", name="Effect Select 2", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select_3": MapItem(
+        key="effect_select_3", name="Effect Select 3", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select_4": MapItem(
+        key="effect_select_4", name="Effect Select 4", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select_5": MapItem(
+        key="effect_select_5", name="Effect Select 5", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select_6": MapItem(
+        key="effect_select_6", name="Effect Select 6", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select1": MapItem(
+        key="effect_select1", name="Effect Select 1", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select2": MapItem(
+        key="effect_select2", name="Effect Select 2", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select3": MapItem(
+        key="effect_select3", name="Effect Select 3", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select4": MapItem(
+        key="effect_select4", name="Effect Select 4", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select5": MapItem(
+        key="effect_select5", name="Effect Select 5", icon="mdi:sawtooth-wave"
+    ),
+    "effect_select6": MapItem(
+        key="effect_select6", name="Effect Select 6", icon="mdi:sawtooth-wave"
+    ),
+    "fader_1_mute": MapItem(
+        key="fader_1_mute", name="Fader 1 Mute", icon="mdi:microphone-off"
+    ),
     "fader_1": MapItem(key="fader_1", name="Fader 1", icon="mdi:volume-high"),
-    "fader_2_mute": MapItem(key="fader_2_mute", name="Fader 2 Mute", icon="mdi:microphone-off"),
+    "fader_2_mute": MapItem(
+        key="fader_2_mute", name="Fader 2 Mute", icon="mdi:microphone-off"
+    ),
     "fader_2": MapItem(key="fader_2", name="Fader 2", icon="mdi:volume-high"),
-    "fader_3_mute": MapItem(key="fader_3_mute", name="Fader 3 Mute", icon="mdi:microphone-off"),
+    "fader_3_mute": MapItem(
+        key="fader_3_mute", name="Fader 3 Mute", icon="mdi:microphone-off"
+    ),
     "fader_3": MapItem(key="fader_3", name="Fader 3", icon="mdi:volume-high"),
-    "fader_4_mute": MapItem(key="fader_4_mute", name="Fader 4 Mute", icon="mdi:microphone-off"),
+    "fader_4_mute": MapItem(
+        key="fader_4_mute", name="Fader 4 Mute", icon="mdi:microphone-off"
+    ),
     "fader_4": MapItem(key="fader_4", name="Fader 4", icon="mdi:volume-high"),
-    "fader1_mute": MapItem(key="fader1_mute", name="Fader 1 Mute", icon="mdi:microphone-off"),
-    "fader2_mute": MapItem(key="fader2_mute", name="Fader 2 Mute", icon="mdi:microphone-off"),
-    "fader3_mute": MapItem(key="fader3_mute", name="Fader 3 Mute", icon="mdi:microphone-off"),
-    "fader4_mute": MapItem(key="fader4_mute", name="Fader 4 Mute", icon="mdi:microphone-off"),
+    "fader1_mute": MapItem(
+        key="fader1_mute", name="Fader 1 Mute", icon="mdi:microphone-off"
+    ),
+    "fader2_mute": MapItem(
+        key="fader2_mute", name="Fader 2 Mute", icon="mdi:microphone-off"
+    ),
+    "fader3_mute": MapItem(
+        key="fader3_mute", name="Fader 3 Mute", icon="mdi:microphone-off"
+    ),
+    "fader4_mute": MapItem(
+        key="fader4_mute", name="Fader 4 Mute", icon="mdi:microphone-off"
+    ),
     "headphones": MapItem(key="Headphones", name="Headphones", icon="mdi:headphones"),
-    "line_in": MapItem(key="LineIn", name="Line In", icon="mdi:audio-input-stereo-minijack"),
-    "line_out": MapItem(key="LineOut", name="Line In", icon="mdi:audio-input-stereo-minijack"),
-    "LineIn": MapItem(key="LineIn", name="Line In", icon="mdi:audio-input-stereo-minijack"),
-    "LineOut": MapItem(key="LineOut", name="Line In", icon="mdi:audio-input-stereo-minijack"),
-    "mic_monitor": MapItem(key="MicMonitor", name="Microphone Monitor", icon="mdi:microphone-outline"),
+    "line_in": MapItem(
+        key="LineIn", name="Line In", icon="mdi:audio-input-stereo-minijack"
+    ),
+    "line_out": MapItem(
+        key="LineOut", name="Line In", icon="mdi:audio-input-stereo-minijack"
+    ),
+    "LineIn": MapItem(
+        key="LineIn", name="Line In", icon="mdi:audio-input-stereo-minijack"
+    ),
+    "LineOut": MapItem(
+        key="LineOut", name="Line In", icon="mdi:audio-input-stereo-minijack"
+    ),
+    "mic_monitor": MapItem(
+        key="MicMonitor", name="Microphone Monitor", icon="mdi:microphone-outline"
+    ),
     "mic": MapItem(key="Mic", name="Microphone", icon="mdi:microphone"),
     "Mic": MapItem(key="Mic", name="Microphone", icon="mdi:microphone"),
-    "MicMonitor": MapItem(key="MicMonitor", name="Microphone Monitor", icon="mdi:microphone-outline"),
+    "MicMonitor": MapItem(
+        key="MicMonitor", name="Microphone Monitor", icon="mdi:microphone-outline"
+    ),
     "music": MapItem(key="Music", name="Music", icon="mdi:music"),
     "Music": MapItem(key="Music", name="Music", icon="mdi:music"),
     "mute_all": MapItem(key="mute_all", name="Mute All", icon="mdi:microphone-off"),
     "mute_chat": MapItem(key="mute_chat", name="Mute Chat", icon="mdi:microphone-off"),
-    "mute_console": MapItem(key="mute_console", name="Mute Console", icon="mdi:microphone-off"),
-    "mute_line_in": MapItem(key="mute_line_in", name="Mute Line In", icon="mdi:microphone-off"),
-    "mute_line_out": MapItem(key="mute_line_out", name="Mute Line Out", icon="mdi:microphone-off"),
-    "mute_mic": MapItem(key="mute_mic", name="Mute Microphone", icon="mdi:microphone-off"),
-    "mute_microphone": MapItem(key="mute_microphone", name="Mute Microphone", icon="mdi:microphone-off"),
-    "mute_music": MapItem(key="mute_music", name="Mute Music", icon="mdi:microphone-off"),
-    "mute_system": MapItem(key="mute_system", name="Mute System", icon="mdi:microphone-off"),
+    "mute_console": MapItem(
+        key="mute_console", name="Mute Console", icon="mdi:microphone-off"
+    ),
+    "mute_line_in": MapItem(
+        key="mute_line_in", name="Mute Line In", icon="mdi:microphone-off"
+    ),
+    "mute_line_out": MapItem(
+        key="mute_line_out", name="Mute Line Out", icon="mdi:microphone-off"
+    ),
+    "mute_mic": MapItem(
+        key="mute_mic", name="Mute Microphone", icon="mdi:microphone-off"
+    ),
+    "mute_microphone": MapItem(
+        key="mute_microphone", name="Mute Microphone", icon="mdi:microphone-off"
+    ),
+    "mute_music": MapItem(
+        key="mute_music", name="Mute Music", icon="mdi:microphone-off"
+    ),
+    "mute_system": MapItem(
+        key="mute_system", name="Mute System", icon="mdi:microphone-off"
+    ),
     "mute": MapItem(key="Mute", name="Mute", icon="mdi:microphone-off"),
     "sample": MapItem(key="Sample", name="Sample", icon="mdi:music-note"),
-    "sampler_bottom_left": MapItem(key="sampler_bottom_left", name="Sampler Bottom Left", icon="mdi:music-note"),
-    "sampler_bottom_right": MapItem(key="sampler_bottom_right", name="Sampler Bottom Right", icon="mdi:music-note"),
-    "sampler_clear": MapItem(key="sampler_clear", name="Sampler Clear", icon="mdi:music-note"),
-    "sampler_select_a": MapItem(key="sampler_select_a", name="Sampler Select A", icon="mdi:music-note"),
-    "sampler_select_b": MapItem(key="sampler_select_b", name="Sampler Select B", icon="mdi:music-note"),
-    "sampler_select_c": MapItem(key="sampler_select_c", name="Sampler Select C", icon="mdi:music-note"),
-    "sampler_top_left": MapItem(key="sampler_top_left", name="Sampler Top Left", icon="mdi:music-note"),
-    "sampler_top_right": MapItem(key="sampler_top_right", name="Sampler Top Right", icon="mdi:music-note"),
+    "sampler_bottom_left": MapItem(
+        key="sampler_bottom_left", name="Sampler Bottom Left", icon="mdi:music-note"
+    ),
+    "sampler_bottom_right": MapItem(
+        key="sampler_bottom_right", name="Sampler Bottom Right", icon="mdi:music-note"
+    ),
+    "sampler_clear": MapItem(
+        key="sampler_clear", name="Sampler Clear", icon="mdi:music-note"
+    ),
+    "sampler_select_a": MapItem(
+        key="sampler_select_a", name="Sampler Select A", icon="mdi:music-note"
+    ),
+    "sampler_select_b": MapItem(
+        key="sampler_select_b", name="Sampler Select B", icon="mdi:music-note"
+    ),
+    "sampler_select_c": MapItem(
+        key="sampler_select_c", name="Sampler Select C", icon="mdi:music-note"
+    ),
+    "sampler_top_left": MapItem(
+        key="sampler_top_left", name="Sampler Top Left", icon="mdi:music-note"
+    ),
+    "sampler_top_right": MapItem(
+        key="sampler_top_right", name="Sampler Top Right", icon="mdi:music-note"
+    ),
     "system": MapItem(key="System", name="System", icon="mdi:music-box-outline"),
     "System": MapItem(key="System", name="System", icon="mdi:music-box-outline"),
 }
--- module/goxlrutilityapi/models/map_item.py
+++ module/goxlrutilityapi/models/map_item.py
@@ -1,10 +1,6 @@
 """GoXLR Utility API: Map Model"""
 from __future__ import annotations
 
-from typing import Optional
-
-from pydantic import Field
-
 from . import DefaultBaseModel
 
 
