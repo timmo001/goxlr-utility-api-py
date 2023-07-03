4cd107f695d128871642f2f1872a14bea815b6c9
Aidan Timson <contact@timmo.xyz>
2023-07-03 11:48:27 +0100

    1.2.1

--- module/goxlrutilityapi/_version.py
+++ module/goxlrutilityapi/_version.py
@@ -7,5 +7,5 @@ Provides goxlrutilityapi version information.
 
 from incremental import Version
 
-__version__ = Version("goxlrutilityapi", 1, 2, 0)
+__version__ = Version("goxlrutilityapi", 1, 2, 1)
 __all__ = ["__version__"]
