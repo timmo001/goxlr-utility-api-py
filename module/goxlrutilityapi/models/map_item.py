6fccfe9298a09c3e975e27916dc40b918cb97f6a
Aidan Timson <atimson123@gmail.com>
2023-07-03 16:31:33 +0100

    Add key to map (#27)

--- module/goxlrutilityapi/models/map_item.py
+++ module/goxlrutilityapi/models/map_item.py
@@ -1,15 +1,12 @@
 """GoXLR Utility API: Map Model"""
 from __future__ import annotations
 
-from typing import Optional
-
-from pydantic import Field
-
 from . import DefaultBaseModel
 
 
 class MapItem(DefaultBaseModel):
     """MapItem Model"""
 
-    name: Optional[str] = Field(None)
-    icon: Optional[str] = Field(None)
+    key: str
+    name: str
+    icon: str
